28b4cddc7fecdf63729297b757c2a88a365392d3
Katya Fadeeva <katfadmail@gmail.com>
2025-12-13 19:38:32 +0200

    forget to add prices. added

--- 00_try.py
+++ 00_try.py
@@ -7,14 +7,17 @@ import pickle as pk
 
 
 class Order:
-    def __init__(self, side:Literal['bid', 'ask'], price:float, volume:int):
+    def __init__(self,
+                 side:Literal['bid', 'ask'], price:float, volume:int):
         self.side = side
         self.volume = volume
+        self.price = price
         self.timestamp = datetime.now()
     
     def get_data(self)->dict:
         return {
             'side': self.side,
+            'price': self.price,
             'volume': self.volume,
             'timestamp': self.timestamp
         }
@@ -22,42 +25,58 @@ class Order:
 
 class OrderBook:
     def __init__(self):
-        self.storage = []
+        self.asks = []
+        self.bids = []
     
     def add(self, order:Order)->None:
-        self.storage.append(order)
+        if order['side']=='ask':
+            self.asks.append(order)
+        else:
+            self.bids.append(order)
     
-    def get_all(self)->list:
-        return [obj.get_data() for obj in self.storage]
+    def get_asks(self)->list:
+        return self.asks
+    
+    def get_bids(self)->list:
+        return self.bids
 
 
 with open('orders', 'rb') as f:
     orders = pk.load(f)
+
+print(orders[0])
     
-print(len(orders))
+#order_book = OrderBook()
+#order_book.add(orders[0])
+#
+#print(order_book.get_asks())
+#print(order_book.get_bids())
 
 
 # Generate Orders
 #n=1_000
-#dtype = [('type', 'U3'), ('price', 'f4'), ('quantity', 'i4')]
+#dtype = [
+#    ('type', 'U3'),
+#    ('price', 'f4'),
+#    ('quantity', 'i4'),
+#    ('timestamp', 'datetime64[ns]')
+#]
 #
 #types = np.random.choice(['bid', 'ask'], n)
 #prices = np.round(np.random.uniform(1, 200, n), 2).astype('f4')
 #quantities = np.random.randint(1, 100, n).astype('i4')
 #
+#timestamps = []
+#for i in range(n):
+#    timestamps.append(datetime.now())
+#    time.sleep(np.round(np.random.uniform(.1, 2.), 2))
+#
 #orders = np.empty(n, dtype=dtype)
 #orders['type'] = types
 #orders['price'] = prices
 #orders['quantity'] = quantities
-#
-#order_book = OrderBook()
-#for order in orders:
-#    order_book.add(Order(*order))
-#    time.sleep(np.round(np.random.uniform(.1, 2.), 2))
-#
-#
-#orders_list = order_book.get_all()
+#orders['timestamp'] = np.array(timestamps)
 #
 #with open('orders', 'wb') as f:
-#    pk.dump(orders_list, f)
+#    pk.dump(orders, f)
     
