5778b1c95f15d3dd3d5ebaf6e373b0b1c99201eb
Katya Fadeeva <katfadmail@gmail.com>
2026-01-21 20:26:10 +0200

    rewrited order class

--- 00_try.py
+++ 00_try.py
@@ -1,25 +1,51 @@
-from typing import Literal, List, Optional, Union
+from typing import Literal, List, Optional, Union, Dict
 from datetime import datetime
+
 import bisect
+import uuid
 
 
 class Order:
     def __init__(self,
-                 side:Literal['bid', 'ask'],
-                 price:float, volume:int):
+                 side: Literal['bid', 'ask'],
+                 volume: Union[int, float],
+                 price: Optional[float]=None,
+                 order_type: Literal['limit', 'market', 'stop']='limit',
+                 stop_price: Optional[float]=None,
+                 time_in_force: Literal['GTC', 'IOC', 'FOK']='GTC',
+                 # GTC (Good Till Cancelled)
+                 # IOC (Immediate Or Cancel)
+                 # FOK (Fill Or Kill)
+                ):
+        
         self.side = side
         self.volume = volume
         self.price = price
+        self.order_type = order_type
+        self.time_in_force = time_in_force
+        
+        if order_type=='market':
+            self.price = None
+        elif order_type in ['limit', 'stop'] and price is None:
+            raise ValueError(f'Price required for {order_type} order')
+        else:
+            self.price = price
+        
+        if order_type=='stop' and stop_price is None:
+            raise ValueError('Stop price required for stop order')
+        else:
+            self.stop_price = stop_price
+        
         self.timestamp = datetime.now()
-        self.id = id(self)
+        self.id = str(uuid.uuid4())
     
     def get(self)->dict:
         return {
+            'id': self.id,
             'side': self.side,
             'price': self.price,
             'volume': self.volume,
             'timestamp': self.timestamp,
-            'id': self.id,
         }
 
 
@@ -85,29 +111,29 @@ class OrderBook:
     
 if __name__ == "__main__":
 #    ob = OrdersStack()
-#    order = Order('ask', 100, 23)
-#    print(order.get())
-    orders = [
-        Order('bid', 117, 23),
-        Order('ask', 100, 49),
-        Order('ask', 120, 12),
-        Order('bid', 120, 8),
-        Order('ask', 115, 21),
-        Order('ask', 117, 19),
-        Order('bid', 100, 11),
-        Order('ask', 100, 5),
-        Order('bid', 115, 5),
-        Order('bid', 100, 10),
-        
-    ]
-    book = OrderBook()
-    for order in orders:
-        book.add(order)
+    order = Order('ask', 100, 23)
+    print(order.get())
+#    orders = [
+#        Order('bid', 117, 23),
+#        Order('ask', 100, 49),
+#        Order('ask', 120, 12),
+#        Order('bid', 120, 8),
+#        Order('ask', 115, 21),
+#        Order('ask', 117, 19),
+#        Order('bid', 100, 11),
+#        Order('ask', 100, 5),
+#        Order('bid', 115, 5),
+#        Order('bid', 100, 10),
+#        
+#    ]
+#    book = OrderBook()
+#    for order in orders:
+#        book.add(order)
 #    print(book.get_best_ask())
 #    print(book.get_best_bid())
     
-    print('=================')
-    print(book.get_price(100))
+#    print('=================')
+#    print(book.get_price(100))
     
     
     
