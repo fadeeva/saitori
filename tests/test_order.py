cb6907e0f3be42747758c231b0b4e4ac5da94358
Katya Fadeeva <katfadmail@gmail.com>
2026-01-25 19:00:38 +0200

    test for stop-order and raise error for creation

--- tests/test_order.py
+++ tests/test_order.py
@@ -35,4 +35,53 @@ def test_order_creation_market():
     assert order.volume == 55
     assert order.order_type == 'market'
     
+
+def test_order_creation_stop():
+    ''' Test for stop-order '''
+    order = Order(
+        side='bid',
+        price=105,
+        stop_price=106.55,
+        volume=150,
+        order_type='stop'
+    )
+
+    assert order.side == 'bid'
+    assert order.price == 105
+    assert order.stop_price == 106.55
+    assert order.volume == 150
+    assert order.order_type == 'stop'
+
+    
+def test_order_creation_validation():
+    ''' Test of validation '''
+    
+    # Limit order without price - ERROR
+    with pytest.raises(ValueError, match='Price required for limit order'):
+        Order(side='bid', volume=100, order_type='limit', price=None)
+    
+    # Stop order without stop_price - ERROR
+    with pytest.raises(ValueError, match='Stop price required for stop order'):
+        Order(side='bid', price=100, volume=100, order_type='stop', stop_price=None)
+    
+    # Stop order without цены - ERROR (even if there is stop_price)
+    with pytest.raises(ValueError, match='Price required for stop order'):
+        Order(side='bid', price=None, stop_price=100, volume=100, order_type='stop')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
