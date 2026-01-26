3b812722840909fffadcfada7e53d6d51aca2b5e
Katya Fadeeva <katfadmail@gmail.com>
2026-01-26 19:31:36 +0200

    tests for execution of order

--- tests/test_order.py
+++ tests/test_order.py
@@ -64,11 +64,37 @@ def test_order_creation_validation():
     with pytest.raises(ValueError, match='Stop price required for stop order'):
         Order(side='bid', price=100, volume=100, order_type='stop', stop_price=None)
     
-    # Stop order without цены - ERROR (even if there is stop_price)
+    # Stop order without price - ERROR (even if there is stop_price)
     with pytest.raises(ValueError, match='Price required for stop order'):
         Order(side='bid', price=None, stop_price=100, volume=100, order_type='stop')
     
+
+def test_order_execution():
+    ''' Test for partial execution '''
+    order = Order(side='bid', price=100, volume=100)
     
+    order.execute(volume=30, price=99.95)
+    
+    assert order.executed_volume == 30
+    assert order.remaining_volume == 70
+    assert order.status == 'partially_filled'
+    assert hasattr(order, 'last_execution_price')
+    assert order.last_execution_price == 99.95
+    
+    order.execute(volume=70, price=100.10)
+    
+    assert order.executed_volume == 100
+    assert order.remaining_volume == 0
+    assert order.status == 'filled'
+
+
+def test_order_execution_validation():
+    ''' Test of execution validation '''
+    order = Order(side='bid', price=100, volume=100)
+    
+    # Try to execute more that it has
+    with pytest.raises(ValueError, match='Cannot execute 150, remaining: 100'):
+        order.execute(volume=150, price=100)
     
     
     
