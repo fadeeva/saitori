2c3f5f080be15fdec867b3c79700846e24dc91d6
Katya Fadeeva <katfadmail@gmail.com>
2026-02-22 18:12:36 +0200

    iteration for OrderStack, execute FOK

--- src/orderbook.py
+++ src/orderbook.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from typing import Literal, List, Optional, Union, Dict, Tuple
+from typing import Literal, List, Optional, Union, Dict, Tuple, Iterator
 import bisect
 
 from src.order import Order, OrderStatus, OrderType, OrderSide, OrderTIF, OrderErrorMessages
@@ -17,6 +17,18 @@ class OrdersStack:
     
     def show(self) -> List[dict]:
         return [o.get() for o in self._orders]
+    
+    def __iter__(self) -> Iterator[Order]:
+        self._index = 0
+        return self
+    
+    def __next__(self) -> Order:
+        if self._index < len(self._orders):
+            result = self._orders[self._index]
+            self._index += 1
+            return result
+        else:
+            raise StopIteration
     
     def __len__(self) -> int:
         return len(self._orders)
@@ -50,6 +62,7 @@ class OrderBook:
         self.asks = AskOrders()
         self.bids = BidOrders()
     
+    
     def add(self, order: Order) -> None:
         if order.order_type not in [OrderType.LIMIT, OrderType.STOP]:
             return
@@ -60,23 +73,39 @@ class OrderBook:
             opposite_side, same_side = self.asks, self.bids
         
         best_opposite = opposite_side.peek()
+
         if best_opposite:
+            if order.time_in_force is OrderTIF.FOK and not self._is_enough_volume(order, opposite_side):
+                return
+                
             while order.remaining_volume \
                   and opposite_side.peek() \
                   and self._best_or_equal(order, opposite_side.peek().price):
                 self._execute_matched_orders(order, opposite_side.peek(), same_side, opposite_side)
             
-            if order.remaining_volume > 0 and order.time_in_force not in [OrderTIF.IOC, OrderTIF.FOK]:
+            if order.remaining_volume > 0 and order.time_in_force is not OrderTIF.IOC:
                 same_side.push(order)
         else:
-            if order.time_in_force not in [OrderTIF.IOC, OrderTIF.FOK]:
+            if order.time_in_force is not OrderTIF.IOC:
                 same_side.push(order)
+
+                
+    def _is_enough_volume(self, order: Order, opposite_side: List[Order]) -> bool:
+        volume = 0
+        for o in opposite_side:
+            if self._best_or_equal(order, o.price):
+                volume += o.volume
+            else:
+                break
+        return order.volume <= volume
+    
     
     def _best_or_equal(self, order: Order, opposite_price: Decimal) -> bool:
         if order.side == OrderSide.ASK:
             return order.price <= opposite_price
         else:
             return order.price >= opposite_price
+    
     
     def _execute_matched_orders(self,
                                 incoming: Order, existing: Order,
@@ -92,16 +121,20 @@ class OrderBook:
         if existing.remaining_volume == 0:
             opposite_side.pop()
     
+    
     @property
     def best_ask(self) -> Optional[Order]:
         return self.asks.peek()
+    
     
     @property
     def best_bid(self) -> Optional[Order]:
         return self.bids.peek()
     
+    
     def __len__(self):
         return len(self.asks) + len(self.bids)
+    
     
     def __str__(self):
         return f'{str(self.asks)}, {str(self.bids)}'
