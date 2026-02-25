3477bc7b58eed3ee723982143e79e9a35d2be441
Katya Fadeeva <katfadmail@gmail.com>
2026-02-25 22:06:37 +0200

    started Trade&TradesBook

---
+++ src/tradesbook.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+from datetime import datetime
+
+from order import Order, OrderSide
+
+
+class Trade:
+    def __init__(self,
+                 bid_order_id: str, ask_order_id: str,
+                 aggressor_side: OrderSide,
+                 price: Decimal, volume: Decimal):
+        self.bid_order_id = bid_order_id
+        self.ask_order_id = ask_order_id
+        self.side = aggressor_side
+        self.price = price
+        self.volume = volume
+        self.timestamp = datetime.now()
+    
+
+class TradesBook:
+    def __init__(self):
+        self._trades = []
+    
+    def add(self, trade: Trade) -> None:
+        self._trades.append(trade)
+
+        
+trade = Trade('123', '321', OrderSide.ASK, Decimal('110.99'), Decimal('23'))
+
+trades_book = TradesBook()
+
+trades_book.add(trade)
