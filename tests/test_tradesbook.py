135bae72fd1075e47cd7392587295d29e3d5a112
Katya Fadeeva <katfadmail@gmail.com>
2026-02-28 22:01:42 +0200

    simplified test. changed order for mock data

--- tests/test_tradesbook.py
+++ tests/test_tradesbook.py
@@ -2,28 +2,15 @@ import pytest
 
 from decimal import Decimal
 from datetime import datetime
+import uuid
 
 from src.order import *
 from src.tradesbook import *
 
 
 def test_trade_creation():
-    bid = Order(
-        side=OrderSide.BID,
-        price=Decimal('10.54'),
-        volume=Decimal('100'),
-        order_type=OrderType.LIMIT
-    )
-    
-    ask = Order(
-        side=OrderSide.ASK,
-        price=Decimal('10.54'),
-        volume=Decimal('100'),
-        order_type=OrderType.LIMIT
-    )
-    
     trade = Trade(
-        bid.id, ask.id,
+        str(uuid.uuid4()), str(uuid.uuid4()),
         OrderSide.ASK,
         Decimal('10.54'), Decimal('100')
     )
@@ -32,3 +19,8 @@ def test_trade_creation():
     assert trade.price == Decimal('10.54')
     assert trade.side == OrderSide.ASK
     assert trade.volume == Decimal('100')
+    
+
+def test_tradebook_creation():
+    pass
+    
