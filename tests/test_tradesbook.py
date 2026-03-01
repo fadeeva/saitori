0afd73c95214340e43a3ff67061f264a0f509a02
Katya Fadeeva <katfadmail@gmail.com>
2026-03-01 20:03:17 +0200

    rewrite test for trade creation

--- tests/test_tradesbook.py
+++ tests/test_tradesbook.py
@@ -9,15 +9,29 @@ from src.tradesbook import *
 
 
 def test_trade_creation():
+    order_a = Order(
+        side=OrderSide.BID,
+        price=10.54,
+        volume=100,
+        order_type=OrderType.LIMIT
+    )
+    
+    order_b = Order(
+        side=OrderSide.ASK,
+        price=10.54,
+        volume=200,
+        order_type=OrderType.LIMIT
+    )
+    
     trade = Trade(
-        str(uuid.uuid4()), str(uuid.uuid4()),
-        OrderSide.ASK,
+        order_a, order_b,
+        OrderSide.BID,
         Decimal('10.54'), Decimal('100')
     )
     
     assert isinstance(trade.timestamp, datetime)
     assert trade.price == Decimal('10.54')
-    assert trade.side == OrderSide.ASK
+    assert trade.side is not OrderSide.ASK
     assert trade.volume == Decimal('100')
     
 
