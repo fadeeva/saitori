ff356ac108876cc7c3918e74819cbf03216b553f
Katya Fadeeva <katfadmail@gmail.com>
2026-04-09 20:35:04 +0200

    move matching orders to separate class

--- src/orderbook/limit_orderbook.py
+++ src/orderbook/limit_orderbook.py
@@ -10,85 +10,35 @@ if TYPE_CHECKING:
 from src.order import OrderSide, OrderTIF
 from src.orderbook.limit_orders_stack import AskOrders, BidOrders
 
+from src.orderbook.matching_engine import MatchingEngine
+
 
 class OrderBook:
     '''Order book matching bids and asks with price-time priority.'''
     
-    def __init__(self):
-        self.asks = AskOrders()
-        self.bids = BidOrders()
+    def __init__(self):        
+        self.me = MatchingEngine(AskOrders(), BidOrders())
     
     def add(self, order: 'Order') -> None:
-        if order.side == OrderSide.ASK:
-            opposite_side, same_side = self.bids, self.asks
-        else:
-            opposite_side, same_side = self.asks, self.bids
-        
-        best_opposite = opposite_side.peek()
-
-        if best_opposite:
-            if order.time_in_force is OrderTIF.FOK and not self._is_enough_volume(order, opposite_side):
-                order.cancel()
-                return
-                
-            while order.remaining_volume \
-                  and opposite_side.peek() \
-                  and self._best_or_equal(order, opposite_side.peek().price):
-                self._execute_matched_orders(order, opposite_side.peek(), opposite_side)
-                
-        if order.remaining_volume > 0 and order.time_in_force not in [OrderTIF.IOC, OrderTIF.FOK]:
-            same_side.push(order)
-
-                
-    def _is_enough_volume(self, order: 'Order', opposite_side: List['Order']) -> bool:
-        existing_volume = 0
-        for o in reversed(opposite_side):
-            if self._best_or_equal(order, o.price):
-                existing_volume += o.volume
-            else:
-                break
-
-        return order.volume <= existing_volume
-    
-    
-    def _best_or_equal(self, order: 'Order', opposite_price: Decimal) -> bool:
-        if order.side == OrderSide.ASK:
-            return order.price <= opposite_price
-        else:
-            return order.price >= opposite_price
-    
-    
-    def _execute_matched_orders(self,
-                                incoming: 'Order', existing: 'Order',
-                                opposite_side: 'OrdersStack') -> None:
-        
-        volume = min(incoming.remaining_volume, existing.remaining_volume)
-        price = existing.price
-        
-        incoming.execute(volume=volume, price=price)
-        existing.execute(volume=volume, price=price)
-        
-        if existing.remaining_volume == 0:
-            opposite_side.pop()
-        
+        self.me.add(order)
     
     def get_bid_levels(self, depth: int=5) -> List[Tuple[Decimal, Decimal]]:
-        return self.bids.get_levels(depth)
+        return self.me.bids.get_levels(depth)
     
     def get_ask_levels(self, depth: int=5) -> List[Tuple[Decimal, Decimal]]:
-        return self.asks.get_levels(depth)
+        return self.me.asks.get_levels(depth)
     
     def clear(self) -> None:
-        self.asks.clear()
-        self.bids.clear()
+        self.me.asks.clear()
+        self.me.bids.clear()
     
     @property
     def best_ask(self) -> Optional['Order']:
-        return self.asks.peek()
+        return self.me.asks.peek()
     
     @property
     def best_bid(self) -> Optional['Order']:
-        return self.bids.peek()
+        return self.me.bids.peek()
     
     @property
     def spread(self) -> Optional[Decimal]:
@@ -97,10 +47,10 @@ class OrderBook:
         return None
         
     def __len__(self):
-        return len(self.asks) + len(self.bids)
+        return len(self.me.asks) + len(self.me.bids)
     
     def __str__(self):
-        r = [a.__str__() + ' || ' + b.__str__() + '\n' for a, b in zip(self.asks, self.bids)]
+        r = [a.__str__() + ' || ' + b.__str__() + '\n' for a, b in zip(self.me.asks, self.me.bids)]
         return ''.join(r)
             
     
--- src/orderbook/matching_engine.py
+++ src/orderbook/matching_engine.py
@@ -12,7 +12,62 @@ from src.order import OrderSide, OrderTIF
 
 
 class MatchingEngine:
-    def add(self, order: 'Order',
-            asks: Optional['AskOrders', 'AskStopOrders'],
-            bids: Optional['BidOrders', 'BidStopOrders']) -> None:
-        pass
+    
+    def __init__(self,
+                 asks: Union['AskOrders', 'AskStopOrders'],
+                 bids: Union['BidOrders', 'BidStopOrders']):
+        self.asks = asks
+        self.bids = bids
+    
+    def add(self, order: 'Order') -> None:
+        if order.side == OrderSide.ASK:
+            opposite_side, same_side = self.bids, self.asks
+        else:
+            opposite_side, same_side = self.asks, self.bids
+        
+        best_opposite = opposite_side.peek()
+
+        if best_opposite:
+            if order.time_in_force is OrderTIF.FOK and not self._is_enough_volume(order, opposite_side):
+                order.cancel()
+                return
+                
+            while order.remaining_volume \
+                  and opposite_side.peek() \
+                  and self._best_or_equal(order, opposite_side.peek().price):
+                self._execute_matched_orders(order, opposite_side.peek(), opposite_side)
+                
+        if order.remaining_volume > 0 and order.time_in_force not in [OrderTIF.IOC, OrderTIF.FOK]:
+            same_side.push(order)
+
+                
+    def _is_enough_volume(self, order: 'Order', opposite_side: List['Order']) -> bool:
+        existing_volume = 0
+        for o in reversed(opposite_side):
+            if self._best_or_equal(order, o.price):
+                existing_volume += o.volume
+            else:
+                break
+
+        return order.volume <= existing_volume
+    
+    
+    def _best_or_equal(self, order: 'Order', opposite_price: Decimal) -> bool:
+        if order.side == OrderSide.ASK:
+            return order.price <= opposite_price
+        else:
+            return order.price >= opposite_price
+    
+    
+    def _execute_matched_orders(self,
+                                incoming: 'Order', existing: 'Order',
+                                opposite_side: 'OrdersStack') -> None:
+        
+        volume = min(incoming.remaining_volume, existing.remaining_volume)
+        price = existing.price
+        
+        incoming.execute(volume=volume, price=price)
+        existing.execute(volume=volume, price=price)
+        
+        if existing.remaining_volume == 0:
+            opposite_side.pop()
