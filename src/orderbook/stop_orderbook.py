0a762d3a03fc8ec919e72a5439194f918ed2b68d
Katya Fadeeva <katfadmail@gmail.com>
2026-04-13 16:04:00 +0200

    try to figure out w/ stop orders

--- src/orderbook/stop_orderbook.py
+++ src/orderbook/stop_orderbook.py
@@ -1,9 +1,10 @@
 from decimal import Decimal
-from typing import TYPE_CHECKING
+from typing import List, Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .order import Order
 
+from src.order import OrderSide 
 from src.orderbook.stop_orders_stack import AskStopOrders, BidStopOrders
 from src.orderbook.orderbook import OrderBook
 
@@ -13,14 +14,19 @@ class StopOrderBook(OrderBook):
     
     def __init__(self):
         super().__init__(AskStopOrders(), BidStopOrders())
+        self.ask_storage = AskStopOrders()
+        self.bid_storage = BidStopOrders()
     
+    def add_to_storage(self, order: 'Order') -> None:
+        if order.side == OrderSide.ASK:
+            self.ask_storage.push(order)
+        else:
+            self.bid_storage.push(order)
     
-    
-    
-    
-    
-    
+    def get_activated(self, current_price: Decimal) -> Optional[List['Order']]:
+        activated_asks = self.ask_storage.get_activated(current_price)
+        activated_bids = self.bid_storage.get_activated(current_price)
+        
+        # unite, sort by timestamp, add order by order to self.add()
             
-    
-
     
