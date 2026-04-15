577bad681b723f85f77c933d58566a1e9e35d486
Katya Fadeeva <katfadmail@gmail.com>
2026-04-15 14:22:35 +0200

    base for exchange class

---
+++ src/exchange.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+from typing import List, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .order import Order
+
+from src.order import OrderType  
+    
+from src.orderbook.limit_orderbook import LimitOrderBook
+from src.orderbook.stop_orderbook import StopOrderBook
+
+from src.orderlogger import OrderLogger
+from src.tradesbook import TradesBook
+
+
+class Exchange:
+    def __init__(self):
+        self.limit_orderbook = LimitOrderBook()
+        self.stop_orderbook = StopOrderBook()
+        
+        self.logger = OrderLogger()
+        self.tradesbook = TradesBook()
+    
+    def push(self, order: 'Order') -> None:
+        if order.order_type == OrderType.LIMIT:
+            self.limit_orderbook.add(order)
+        elif order.order_type == OrderType.STOP:
+            self.stop_orderbook.add_to_storage(order)
+        else:
+            pass # MARKET
