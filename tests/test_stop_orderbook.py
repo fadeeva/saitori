4377b0f9abefd7fd39b5deaa7cddf73e221bce70
Katya Fadeeva <katfadmail@gmail.com>
2026-04-23 20:58:24 +0200

    test: add orders to stop_orderbook storage

---
+++ tests/test_stop_orderbook.py
@@ -0,0 +1,39 @@
+import pytest
+from datetime import datetime
+
+from src.order import *
+from src.orderbook.stop_orderbook import *
+from src.orderlogger import *
+
+
+logger = OrderLogger()
+
+def test_orderbook_creation():
+    stop_buy = Order( # LIMIT
+        side=OrderSide.BID,
+        stop_price=Decimal('100.50'),
+        price=Decimal('105.50'),
+        volume=Decimal('100'),
+        order_type=OrderType.STOP,
+        time_in_force=OrderTIF.GTC,
+        logger=logger
+    )
+
+    stop_sell = Order( # MARKET
+        side=OrderSide.ASK,
+        stop_price=Decimal('110.00'),
+        volume=Decimal('100'),
+        order_type=OrderType.STOP,
+        time_in_force=OrderTIF.GTC,
+        logger=logger
+    )
+
+    ob = StopOrderBook()
+    ob.add_to_storage(stop_buy)
+    ob.add_to_storage(stop_sell)
+
+    assert ob.storage_len == 2
+    
+    
+
+    
