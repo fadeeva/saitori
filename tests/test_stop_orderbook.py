99f23c04524bc24dd28ca631fbddd39c081d4b0e
Katya Fadeeva <katfadmail@gmail.com>
2026-04-25 21:39:38 +0200

    test: add more orders to stop_orderbook

--- tests/test_stop_orderbook.py
+++ tests/test_stop_orderbook.py
@@ -37,11 +37,11 @@ def test_orderbook_creation():
 def test_orders_activation():
     ob = StopOrderBook()
     
-    for _ in range(5):
+    for i in range(10):
         ob.add_to_storage(
             Order(
-                side=OrderSide.ASK,
-                stop_price=Decimal('100.00'),
+                side=OrderSide.ASK if i%2 else OrderSide.BID,
+                stop_price=Decimal('100.00') if i%2 else Decimal('120.00'),
                 volume=Decimal('20'),
                 order_type=OrderType.STOP,
                 time_in_force=OrderTIF.GTC,
@@ -49,9 +49,9 @@ def test_orders_activation():
             )
         )
 
+    assert ob.storage_len == 10
+    assert len(ob.get_activated(Decimal('100.00'))) == 5
     assert ob.storage_len == 5
-    assert ob.get_activated(Decimal('100.00')) is None
-    assert ob.storage_len == 0
     
     
 
